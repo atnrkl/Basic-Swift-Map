19e2a0095547b4fb027ed1ec5c99973c1cd8b7bb
Ahmet Tanrikulu a00835962 <ahmet.tanrikulu@huawei.com>
2023-08-16 15:58:32 +0300

    Initial Commit

---
+++ developerAppleTutorial1/developerAppleTutorial1App.swift
@@ -0,0 +1,17 @@
+//
+//  developerAppleTutorial1App.swift
+//  developerAppleTutorial1
+//
+//  Created by admin on 16.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct developerAppleTutorial1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
